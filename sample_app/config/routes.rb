e3fac19342e6236a4705bf7d2692cb89aec83fe3
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-12-26 15:59:27 +0900

    change path

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :users
   root "static_pages#home"
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 end
