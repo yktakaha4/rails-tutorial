b35567824a3ad028fb7c1276351c63aabda5d998
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-12-26 21:34:34 +0900

    add index

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
-    validates :name, presence: true
-    validates :email, presence: true
+    validates(
+        :name,
+        presence: true,
+        length: { maximum: 50 },
+    )
+    validates(
+        :email,
+        presence: true,
+        length: { maximum: 255 },
+        format: { with: URI::MailTo::EMAIL_REGEXP },
+        uniqueness: { case_sensitive: false },
+    )
 end
--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_26_102924) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_26_122741) do
 # Could not dump table "sqlite_stat1" because of following StandardError
 #   Unknown type '' for column 'tbl'
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_26_102924) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
