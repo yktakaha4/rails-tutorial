ce85a12becfc6bd5dde28f405a1bc5ff2a7387f4
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-12-26 22:20:01 +0900

    update max length

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
     validates(
         :password,
         presence: true,
-        length: { minimum: 6 },
+        length: { minimum: 8 },
     )
     has_secure_password
 end
