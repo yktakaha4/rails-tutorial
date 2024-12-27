54a9c23300a8ce64462c2d71d82a9c67efb743dd
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-12-27 12:07:05 +0900

    add error messages

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -6,4 +6,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      puts "User saved"
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
