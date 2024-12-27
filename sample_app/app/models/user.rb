e271ad4198b800c270ba6077257898b11aa38350
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-12-27 20:20:09 +0900

    add remember_digest

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
     before_save { self.email = email.downcase }
     validates(
         :name,
@@ -22,5 +23,14 @@ class User < ApplicationRecord
     def User.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
+    end
+
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
     end
 end
