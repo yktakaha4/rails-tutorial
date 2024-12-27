7d5468276b6c5b6fb171e02008bdb67482d66378
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-12-27 10:05:27 +0900

    add avatar

---
+++ sample_app/test/helpers/users_helper_test.rb
@@ -0,0 +1,17 @@
+require './test/test_helper'
+
+class UsersHelperTest < ActionView::TestCase
+  test "gravatar_for" do
+    name = "Michael Hartl"
+    email = "michael@example.com"
+    email_hash = Digest::MD5::hexdigest(email.downcase)
+
+    user = User.new(
+      name: name,
+      email: email,
+    )
+    actual = gravatar_for(user)
+    expected = "<img alt=\"#{name}\" class=\"gravatar\" src=\"https://secure.gravatar.com/avatar/#{email_hash}?s=50\" />"
+    assert_equal expected, actual
+  end
+end
