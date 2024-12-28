ab3654bf25a8c954efb88fb970e2995a02412e2f
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-12-28 16:31:05 +0900

    add users

--- sample_app/test/helpers/users_helper_test.rb
+++ sample_app/test/helpers/users_helper_test.rb
@@ -14,4 +14,18 @@ class UsersHelperTest < ActionView::TestCase
     expected = "<img alt=\"#{name}\" class=\"gravatar\" src=\"https://secure.gravatar.com/avatar/#{email_hash}?s=50\" />"
     assert_equal expected, actual
   end
+
+  test "gravatar_for with size" do
+    name = "Michael Hartl"
+    email = "michael@example.com"
+    email_hash = Digest::MD5::hexdigest(email.downcase)
+
+    user = User.new(
+      name: name,
+      email: email,
+    )
+    actual = gravatar_for(user, size: 80)
+    expected = "<img alt=\"#{name}\" class=\"gravatar\" src=\"https://secure.gravatar.com/avatar/#{email_hash}?s=80\" />"
+    assert_equal expected, actual
+  end
 end
