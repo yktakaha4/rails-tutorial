976e7406ac4daf8d70723b0be3111369fb8d0e31
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-12-28 16:54:55 +0900

    add seeds

--- sample_app/db/seeds.rb
+++ sample_app/db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+password = "piyopiyo"
+
+users = [
+  {
+    name: "Michael Hartl",
+    email: "example@railstutorial.jp",
+    password: password,
+    password_confirmation: password,
+  },
+]
+
+99.times do
+  users << {
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: password,
+    password_confirmation: password,
+  }
+end
+
+print "Creating users"
+users.each.with_index do |user, i|
+  User.create!(user)
+  print "."
+end
+puts ""
+puts "Done!"
