a5edfbdc96a24ba3a0840c89772443ea9aa7c0af
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-12-28 16:05:33 +0900

    more secure

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user&.authenticate(params[:session][:password])
+      forwarding_url = session[:forwarding_url]
       reset_session
       if params[:session][:remember_me] == '1'
         remember @user
@@ -12,7 +13,7 @@ class SessionsController < ApplicationController
         forget @user
       end
       log_in @user
-      redirect_to @user
+      redirect_to forwarding_url || @user
     else
       flash.now[:danger] = "Invalid email/password combination"
       render 'new', status: :unprocessable_entity
--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -40,4 +40,12 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+
+  def current_user?(user)
+    user && user == current_user
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
