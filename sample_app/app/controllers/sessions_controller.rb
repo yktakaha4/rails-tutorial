781e85a99f3550ce03662aa8e951944344557f76
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-12-28 12:32:33 +0900

    refacgtor

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -3,16 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user&.authenticate(params[:session][:password])
       reset_session
       if params[:session][:remember_me] == '1'
-        remember user
+        remember @user
       else
-        forget user
+        forget @user
       end
-      log_in user
-      redirect_to user
+      log_in @user
+      redirect_to @user
     else
       flash.now[:danger] = "Invalid email/password combination"
       render 'new', status: :unprocessable_entity
--- sample_app/test/integration/users_login_test.rb
+++ sample_app/test/integration/users_login_test.rb
@@ -77,7 +77,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with remembering" do
     log_in_as(@user, remember_me: "1")
-    assert_not cookies[:remember_token].blank?
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
   end
 
   test "login without remembering" do
