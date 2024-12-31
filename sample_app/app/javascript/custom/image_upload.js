ce03a7515ab57c58da7f4fbacffe2eb4ab76197f
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-12-31 23:57:23 +0900

    add user microposts

---
+++ sample_app/app/javascript/custom/image_upload.js
@@ -0,0 +1,12 @@
+document.addEventListener("turbo:load", () => {
+    document.addEventListener("change", (event) => {
+        const image_upload = document.getElementById("micropost_image")
+        if (image_upload && image_upload.files.length > 0) {
+            const size_in_megabytes = image_upload.files[0].size / 1024 / 1024
+            if (size_in_megabytes > 5) {
+                alert("Maximum file size is 5MB. Please choose a smaller file.")
+                image_upload.value = ""
+            }
+        }
+    })
+})
