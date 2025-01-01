1174a7235793ef8ec206226c9ee1b6a472a4b534
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2025-01-01 20:40:13 +0900

    add test

--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -103,7 +103,10 @@ class UserTest < ActiveSupport::TestCase
     assert_not michael.following?(archer)
     michael.follow(archer)
     assert michael.following?(archer)
+    assert archer.followers.include?(michael)
     michael.unfollow(archer)
     assert_not michael.following?(archer)
+    michael.follow(michael)
+    assert_not michael.following?(michael)
   end
 end
